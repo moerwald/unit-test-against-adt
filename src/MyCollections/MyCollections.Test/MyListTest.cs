07fbeea0ffc9d858b062864ddcbd4ac4a3a23aad
moerwald <andreas.mewald@gmail.com>
2020-04-20 10:08:13 +0200

    - Added new classes and tests.

---
+++ src/MyCollections/MyCollections.Test/MyListTest.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace MyCollections.Test
+{
+    public class MyListTest : IMyListTest<int>
+    {
+        protected override void AssertAreEqual(int val1, int val2)
+            => Assert.Equal(val1, val2);
+
+        protected override IMyList<int> CreateSut() => new MyList<int>();
+
+        protected override int GetElement() => 42;
+
+        protected override IEnumerable<int> GetElements()
+            => new List<int> { 1, 2, 3, 4 };
+    }
+}
---
+++ src/MyCollections/MyCollections/MyList.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace MyCollections
+{
+    public class MyList<T> : IMyList<T>
+    {
+        private readonly List<T> _lst = new List<T>();
+        public void Append(T element) => _lst.Add(element);
+
+        public void Clear() => _lst.Clear();
+
+        public int Count() => _lst.Count;
+
+        public T GetElementAt(int index) => _lst[index];
+    }
+}
